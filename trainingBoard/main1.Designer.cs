62f96f568484bb99360fe21d92d4c926a2a4ca32
phineasphreak <riftpp@gmail.com>
2020-03-26 11:07:12 +0100

    Modif Form1 en main1

--- trainingBoard/main1.Designer.cs
+++ trainingBoard/main1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace trainingBoard
 {
-    partial class Form1
+    partial class main1
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // main1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "main1";
+            this.Text = "main1";
+            this.Load += new System.EventHandler(this.main1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
---
+++ trainingBoard/main1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trainingBoard
+{
+    public partial class main1 : Form
+    {
+        public main1()
+        {
+            InitializeComponent();
+        }
+
+        private void main1_Load(object sender, EventArgs e)
+        {
+            // Variables :
+            string current_user = Environment.UserName;
+            DateTime current_date = DateTime.Now;
+
+            string user_and_date = ($" - {current_user} {current_date}");
+
+            // Initialisation main1
+            this.Text = "trainingBoard - Gestion des Formation" + user_and_date;
+        }
+    }
+}
