bd32629740b9cf750f4593594a8523859121ef3f
phineasphreak <riftpp@gmail.com>
2020-04-04 20:41:58 +0200

    Creation UserForm MutualFormationID et premier design UserForm

---
+++ trainingBoard/MutualFormationID.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace trainingBoard
+{
+    partial class MutualFormationID
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MutualFormationID));
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxIdentifiant = new System.Windows.Forms.TextBox();
+            this.textBoxMotDePasse = new System.Windows.Forms.TextBox();
+            this.buttonValidation = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.BackColor = System.Drawing.Color.LightCoral;
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.Location = new System.Drawing.Point(297, 451);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 1;
+            this.buttonClose.Text = "Fermer";
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonValidation);
+            this.groupBox1.Controls.Add(this.textBoxMotDePasse);
+            this.groupBox1.Controls.Add(this.textBoxIdentifiant);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 118);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(360, 250);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Identification requise";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Identifiant :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mot de Passe :";
+            // 
+            // textBoxIdentifiant
+            // 
+            this.textBoxIdentifiant.Location = new System.Drawing.Point(145, 101);
+            this.textBoxIdentifiant.Name = "textBoxIdentifiant";
+            this.textBoxIdentifiant.Size = new System.Drawing.Size(185, 21);
+            this.textBoxIdentifiant.TabIndex = 0;
+            // 
+            // textBoxMotDePasse
+            // 
+            this.textBoxMotDePasse.Location = new System.Drawing.Point(145, 128);
+            this.textBoxMotDePasse.Name = "textBoxMotDePasse";
+            this.textBoxMotDePasse.PasswordChar = '•';
+            this.textBoxMotDePasse.Size = new System.Drawing.Size(185, 21);
+            this.textBoxMotDePasse.TabIndex = 1;
+            // 
+            // buttonValidation
+            // 
+            this.buttonValidation.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.buttonValidation.FlatAppearance.BorderSize = 2;
+            this.buttonValidation.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonValidation.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonValidation.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.buttonValidation.Location = new System.Drawing.Point(255, 185);
+            this.buttonValidation.Name = "buttonValidation";
+            this.buttonValidation.Size = new System.Drawing.Size(75, 23);
+            this.buttonValidation.TabIndex = 2;
+            this.buttonValidation.Text = "Valider";
+            this.buttonValidation.UseVisualStyleBackColor = true;
+            this.buttonValidation.Click += new System.EventHandler(this.buttonValidation_Click);
+            // 
+            // MutualFormationID
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 486);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MutualFormationID";
+            this.Text = "MutualFormationID";
+            this.Load += new System.EventHandler(this.MutualFormationID_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxMotDePasse;
+        private System.Windows.Forms.TextBox textBoxIdentifiant;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonValidation;
+    }
+}
---
+++ trainingBoard/MutualFormationID.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trainingBoard
+{
+    public partial class MutualFormationID : Form
+    {
+        public MutualFormationID()
+        {
+            InitializeComponent();
+        }
+
+        private void MutualFormationID_Load(object sender, EventArgs e)
+        {
+            //
+            // Initialisation MutualFormationID
+            //
+            this.Text = "Mutuelle Formation - Identification";
+            
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonValidation_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxIdentifiant.Text) | string.IsNullOrEmpty(textBoxMotDePasse.Text))
+            {
+                MessageBox.Show("Avertissement," + (Char)13 + 
+                    "Identifiant ou Mot de Passe Vide", "Avertissement saisie incorrecte", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
